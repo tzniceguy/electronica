150da03d48348e1aace4c8086820a192a0b98c4f
Tzniceguy <joeshimbi@gmail.com>
2023-08-04 23:40:35 +0300

    modified:   db.sqlite3 	modified:   electronica/__pycache__/urls.cpython-311.pyc 	modified:   electronica/urls.py 	new file:   products/affairs.jpg 	modified:   shop/__pycache__/models.cpython-311.pyc 	modified:   shop/__pycache__/urls.cpython-311.pyc 	modified:   shop/templates/base.html 	modified:   shop/templates/index.html 	modified:   shop/templates/product_detail.html 	modified:   shop/templates/product_list.html

--- electronica/urls.py
+++ electronica/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    #path('', include('shop.urls')),
+    path('', include('shop.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('shop/', include('shop.urls')),
